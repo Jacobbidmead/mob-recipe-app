4b67aa14ce2f818666b2f9b545a2278d8562bd39
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-05-27 13:42:01 +0100

    feat: created fetchData function in Recipes

--- app/Recipes.tsx/page.tsx
+++ app/Recipes.tsx/page.tsx
@@ -1,10 +1,53 @@
 import { RecipeCard } from "../components/RecipeCard";
 
-export const Recipes: React.FC = () => {
+interface Recipes {
+  title: string;
+  url: string;
+  id: number;
+}
+
+const fetchData = async (): Promise<Recipes[]> => {
+  const username = "mob-api";
+  const password = "9r7rey5567ce0m7hbt1u";
+  const url = "https://api.mob.co.uk/task/recipes.json";
+
+  if (!username || !password || !url) {
+    console.error("Missing environment variables");
+    throw new Error("Missing environment variables");
+  }
+
+  const headers = new Headers();
+  headers.set("Authorization", "Basic " + btoa(username + ":" + password));
+
+  console.log("Fetching data from:", url);
+  console.log("Authorization Header:", headers.get("Authorization"));
+
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: headers,
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    console.log("Fetched data:", data);
+
+    const recipeData: Recipes[] = data;
+
+    return recipeData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+export const Recipes: React.FC = async () => {
+  const recipes = await fetchData();
   return (
     <>
       <div className="grid grid-cols-4">
-        <RecipeCard />
+        <RecipeCard recipes={recipes} />
       </div>
     </>
   );
--- app/components/RecipeCard.tsx
+++ app/components/RecipeCard.tsx
@@ -1,10 +1,12 @@
-export const RecipeCard: React.FC = () => {
+"use client";
+
+export const RecipeCard: React.FC = (recipes) => {
   return (
     <>
       <div className="border-black border-solid border-2 p-10">
-        <div>Title</div>
-        <img src=" alt=" />
-        <button>Bookmark</button>
+        <div>{recipes.title}</div>
+        <img src="" alt="" />
+        <button>Bookmark </button>
       </div>
     </>
   );
