30354e2ee312e41f5eeb310100e07c80642f40a4
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-05-27 15:15:45 +0100

    feat: added bookmarked items to UI with interface

--- app/components/Bookmarks.tsx
+++ app/components/Bookmarks.tsx
@@ -1,3 +1,37 @@
-export const Bookmarks: React.FC = () => {
-  return <>Bookmarks</>;
+"use client";
+
+import React from "react";
+import { RecipeCard } from "./RecipeCard";
+
+interface Recipe {
+  id: number;
+  title: string;
+  slug: string;
+  image: Image[];
+}
+
+interface Image {
+  id: number;
+  title: string;
+  url: string;
+  width: string;
+  height: string;
+}
+
+interface BookmarksProps {
+  bookmarkedRecipes: Recipe[];
+}
+
+export const Bookmarks: React.FC<BookmarksProps> = ({ bookmarkedRecipes }) => {
+  if (bookmarkedRecipes.length === 0) {
+    return <div>No bookmarked recipes</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {bookmarkedRecipes.map((recipe) => (
+        <RecipeCard key={recipe.id} recipe={recipe} onBookmark={() => {}} />
+      ))}
+    </div>
+  );
 };
